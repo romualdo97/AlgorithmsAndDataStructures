c0cec1084ff6e9db697ac9b8e1da00d331828adf
Romualdo VIllalobos <romualdorojo97@gmail.com>
2025-09-29 23:05:28 -0500

    Refreshing on bubble sort and heapsort

---
+++ bubble_sort.cpp
@@ -0,0 +1,20 @@
+﻿#include <algorithm>
+#include <iterator>
+
+int main()
+{
+    int Data[]{ 20, 33, 12, 53, 24, 65, 23, 4, 53, 1 };
+
+    for (int k = std::size(Data); k > 0; --k)
+    {
+        for (int i = 0; i < k - 1; ++i) // k - 1 to avoid null checking when comparing BeforeLastIndex and LastIndex
+        {
+            if (Data[i] > Data[i + 1])
+            {
+                std::swap(Data[i], Data[i + 1]);    
+            }
+        }
+    }
+
+    return -1;
+}
---
+++ heapsort.cpp
@@ -0,0 +1,65 @@
+﻿#include <algorithm>
+#include <iterator>
+// https://www.quora.com/How-is-STLs-std-make_heap-implemented
+
+void SiftDown(int* Arr, int Size, int ParentIndex)
+{
+    int LargestValueAtIndex = ParentIndex; // Assume larger is the ParentIndex
+    int LeftChildIndex = ParentIndex * 2 + 1;
+    int RightChildIndex = ParentIndex * 2 + 2;
+
+    // Is left child larger than parent?
+    if (LeftChildIndex < Size && Arr[LeftChildIndex] > Arr[LargestValueAtIndex])
+    {
+        LargestValueAtIndex = LeftChildIndex;
+    }
+
+    // Is right child larger than left?
+    if (RightChildIndex < Size && Arr[RightChildIndex] > Arr[LargestValueAtIndex])
+    {
+        LargestValueAtIndex = RightChildIndex;
+    }
+
+    // Should parent index be updated?
+    if (ParentIndex != LargestValueAtIndex)
+    {
+        std::swap(Arr[ParentIndex], Arr[LargestValueAtIndex]);
+
+        // Sift down from the index where it was the node with the largest value
+        // useful in cases like
+        //                      index
+        //      (20)            0,
+        //      /  \
+        //    50   (57)         1, 2,
+        //   /  \    |
+        // 47   50   33         3, 4, 5
+        // Here when 20 and 57 get swapped, we will end with 20 -> 33 not satisfying
+        // the max heap property, so we should start a new SiftDown operation from the new node located at index [2]
+        SiftDown(Arr, Size, LargestValueAtIndex);
+    }
+}
+
+int main()
+{
+    int Data[]
+    {
+                20,
+        22,             33,
+        47, 50,         57
+    };
+
+    for (int i = (std::size(Data) - 2) / 2; i >= 0; --i)
+    {
+        SiftDown(Data, std::size(Data), i);        
+    }
+    // SiftDown(Data, std::size(Data), 2);
+    // SiftDown(Data, std::size(Data), 1);
+    // SiftDown(Data, std::size(Data), 0);
+
+    // std::make_heap(Data, Data + std::size(Data));
+    // SiftDown(Data, std::size(Data), 2);
+    // SiftDown(Data, std::size(Data), 1);
+    // SiftDown(Data, std::size(Data), 0);
+
+    return -1;
+}
