028478e73925472ad988de04d2df2819cdb00d9c
Romualdo VIllalobos <romualdorojo97@gmail.com>
2026-03-28 12:09:02 -0500

    Added simple false sharing experiment

--- CodeChallenges/Sorting/bubble_sort.cpp
+++ CodeChallenges/Sorting/bubble_sort.cpp
@@ -5,8 +5,10 @@ int main()
 {
     int Data[]{ 20, 33, 12, 53, 24, 65, 23, 4, 53, 1 };
 
-    for (int k = std::size(Data); k > 0; --k)
+    // Find the biggest element in a buffer of k elements
+    for (int k = std::size(Data); k >= 2; --k)
     {
+        // Find the biggest element and move it to last position of the buffer
         for (int i = 0; i < k - 1; ++i) // k - 1 to avoid null checking when comparing BeforeLastIndex and LastIndex
         {
             if (Data[i] > Data[i + 1])
--- Threading/false_sharing.cpp
+++ Threading/false_sharing.cpp
@@ -1,3 +1,34 @@
-﻿//
-// Created by rojov on 22/03/2026.
-//
+﻿#include <thread>
+#include <cstdint>
+
+#define CACHE_LINE_BYTES 0
+
+struct alignas(CACHE_LINE_BYTES) Counter
+{
+    uint32_t Value{0};
+};
+
+Counter PerThreadCounters[] = { {}, {}, {}, {} };
+
+void IncrementWorker(Counter* Counter)
+{
+    while (Counter->Value != pow(2,28))
+    {
+        ++(Counter->Value);    
+    }
+}
+
+int main()
+{
+    std::thread ThreadA(IncrementWorker, PerThreadCounters);
+    std::thread ThreadB(IncrementWorker, PerThreadCounters + 1);
+    std::thread ThreadC(IncrementWorker, PerThreadCounters + 2);
+    std::thread ThreadD(IncrementWorker, PerThreadCounters + 3);
+
+    ThreadA.join();
+    ThreadB.join();
+    ThreadC.join();
+    ThreadD.join();
+
+    return 0;
+}
