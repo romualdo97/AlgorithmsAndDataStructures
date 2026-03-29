6916b889511e7982fe95db7ec59d9277ac11d3bb
Romualdo VIllalobos <romualdorojo97@gmail.com>
2026-03-28 21:15:11 -0500

    Added cache trashing experiment

---
+++ Threading/cache_associativity.cpp
@@ -0,0 +1,37 @@
+﻿#include <chrono>
+#include <cstdint>
+#include <cstdio>
+#include <ctime>
+#include <iostream>
+
+int main()
+{
+    // Experiment from: https://en.algorithmica.org/hpc/cpu-cache/associativity/
+    // My processor data
+    // Arrow Lake
+    // Cache Level      Size        Associativity       Latency
+    // L0 Data	        48 KB	    12-way          	4 cycles
+    // L0 Instruction	64 KB	    16-way	            -
+    // L1 Data	        192 KB	    12-way	            9 cycles
+    // L2 (Mid-Level)	3 MB*	    10-way	            17 cycles
+    // L3 (Shared)	    3 MB/slice	12-way	            51–84 cycles
+    
+    // Create a big buffer
+    constexpr int N = 1 << 20;
+    constexpr int STEP_SIZE = 1024; // ~18ms and ~8ms if 1025 
+    int* Buffer = new int[N * STEP_SIZE];
+
+    auto Start = std::chrono::high_resolution_clock::now();
+    
+    for (int i = 0; i < N * STEP_SIZE; i += STEP_SIZE)
+    {
+        Buffer[i]++;
+    }
+
+    auto End = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> Duration = End - Start;
+    std::cout << Duration.count() << "ms" << std::endl;
+
+    delete[] Buffer;
+    return 0;
+}
